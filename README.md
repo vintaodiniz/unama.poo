5533a0228fa136d95723df935968082ca98ad94b
vintaodiniz <victortadeudinizkos@gmail.com>
2024-08-28 13:02:48 +0000

    add: codigo balanco trimestral

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # unama.poo
+class
---
+++ unama.java
@@ -0,0 +1,12 @@
+public class BalancoTrimestral{
+    public static void main(String[] args) {
+        int gastojaneiro = 15000;
+        int gastofevereiro = 23000 ;
+        int gastomarço = 17000;
+
+        int gastoTrimeste = gastojaneiro + gastofevereiro + gastomarço;
+
+        System.out.println("O gasto total do primeiro trimeste foi de R$" + gastoTrimeste);
+    }
+}
+    
